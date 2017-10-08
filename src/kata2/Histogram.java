9e1133fd0cd1f245585a86e71cf7df25ef2748e0
Samu96 <srg.dioniso@gmail.com>
2017-10-08 12:09:19 +0100

    Kata Version3

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -4,25 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Histogram {
-    private final int[] array;
+    private final int[] data;
 
 
-    public Histogram(int[] array) {
-        this.array = array;
+    public Histogram(int[] data) {
+        this.data = data;
     }
     
     public int[] getArray() {
-        return array;
+        return data;
     }
     
     public Map<Integer,Integer> getHistogram(){
         Map <Integer,Integer> datos = new HashMap<>();
-        for (int i = 0; i < array.length; i++) {
-            if(datos.containsKey(array[i])){
-                datos.put(array[i], datos.get(array[i])+1);
-            }else{
-                datos.put(array[i], 1);
-            }
+        for (int i : data) {
+            datos.put(i, datos.containsKey(i) ? datos.get(i) + 1 : 1);
         }
         return datos;
     }
--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -6,9 +6,9 @@ import java.util.Map;
 
 public class Kata2 {
     public static void main(String[] args) {
-        int[] array = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+        int[] datos = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
         
-        Histogram histo = new Histogram(array);
+        Histogram histo = new Histogram(datos);
         Map<Integer,Integer> histogr = histo.getHistogram();
         
         for (int key : histogr.keySet()) {
